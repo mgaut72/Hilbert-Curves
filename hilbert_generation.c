9c7cc394a3dbbff0ecab420dbae921bf9ebade7e
Matt Gautreau <mgaut72@gmail.com>
2015-01-21 16:34:15 -0700

    remove globals from hilbert generation to modularize it

--- hilbert_generation.c
+++ hilbert_generation.c
@@ -1,19 +1,11 @@
 #include <stdio.h>
 #include <stdio.h>
 
-int x = -1;     // globals
-int y = 0;
-
-int s = 0;      // distance along the hilbert curve
-
-int blen;       // length to print
-
-
-void step(int dir);
+void step(int dir, int blen, int *s, int *x, int *y);
 void binary(unsigned k, int len, char *s);
-void hilbert(int dir, int rot, int order);
+void hilbert(int dir, int rot, int order, int blen, int *s, int *x, int *y);
 
-void hilbert(int dir, int rot, int order){
+void hilbert(int dir, int rot, int order, int blen, int *s, int *x, int *y){
 /* rot == rotation is either +/- 1 corresponding to clockwise or counter
  * dir = direction = 0,1,2,3 corresponding to right, up, left, down
  * order is the resolution of the curve we are currently at
@@ -34,44 +26,45 @@ void hilbert(int dir, int rot, int order){
      * dir = 0, rot = 1, but this works in general.
      *
      * Note that comments
-     * are based on the assumptions given about the initial conditions
+     * are based on the assumptions given about the initial conditions,
+     * which won't necessarily be true on the recusive steps
      */
 
-    dir = dir + rot;                // set dir to up
-    hilbert(dir, -rot, order - 1);  // draw sub-curve up, counterclockwise
-    step(dir);                      // step up
+    dir = dir + rot;                                // set dir to up
+    hilbert(dir, -rot, order - 1, blen, s, x, y);   // draw sub-curve up, ccw
+    step(dir, blen, s, x, y);                       // step up
 
-    dir = dir - rot;                // set dir to right
-    hilbert(dir, rot, order - 1);   // draw sub-curve right, clockwise
-    step(dir);                      // step right
-    hilbert(dir, rot, order - 1);   // draw sub-curve right, clockwise
+    dir = dir - rot;                                // set dir to right
+    hilbert(dir, rot, order - 1, blen, s, x, y);    // sub-curve right, cw
+    step(dir, blen, s, x, y);                       // step right
+    hilbert(dir, rot, order - 1, blen, s, x, y);    // sub-curve right, cw
 
-    dir = dir - rot;                // set dir to down (its modular)
-    step(dir);                      // step down
-    hilbert(dir, -rot, order - 1);  // draw sub-curve down, counterclockwise
+    dir = dir - rot;                                // set dir to down
+    step(dir, blen, s, x, y);                       // step down
+    hilbert(dir, -rot, order - 1, blen, s, x, y);   // sub-curve down, ccw
 }
 
 
-void step(int dir){
+void step(int dir, int blen, int *s, int *x, int *y){
     char ii[33], xx[17], yy[17];
 
     switch (dir & 3) {      // effectively modding by 4, but always positive
     // alter the coordinates assocaiated with this step
         case 0:
-            x++; break;
+            (*x)++; break;
         case 1:
-            y++; break;
+            (*y)++; break;
         case 2:
-            x--; break;
+            (*x)--; break;
         case 3:
-            y--; break;
+            (*y)--; break;
     }
-    binary(s, 2*blen, ii);
-    binary(x, blen, xx);
-    binary(y, blen, yy);
+    binary(*s, 2*blen, ii);
+    binary(*x, blen, xx);
+    binary(*y, blen, yy);
 
-    printf("%4d : %d %s %s %s\n", s, dir & 3, ii, xx, yy);
-    s++;
+    printf("%4d : %d %s %s %s\n", *s, dir & 3, ii, xx, yy);
+    (*s)++;
 }
 
 
@@ -98,8 +91,13 @@ int main(int argc, char **argv){
         return 1;
     }
 
+    int x = -1, y = 0;          // coordinates
+    int s = 0;                  // distance along curve
+    int blen;                   // # significant digits of s
+
     order = atoi(argv[1]);
     blen = order;
-    hilbert(0,1,order);
+    step(0, blen, &s, &x, &y);
+    hilbert(0,1,order, blen, &s, &x, &y);
     return 0;
 }
