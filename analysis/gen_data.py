2e99539e89ba90ea679ed7ef91c84458e9612243
Matt Gautreau <mgaut72@gmail.com>
2015-03-02 10:51:31 -0700

    add plotting script for results of generation script

--- analysis/gen_data.py
+++ analysis/gen_data.py
@@ -37,25 +37,6 @@ def run_order(n):
     return data
 
 
-#def plot_data(data):
-#    xs = data.keys()
-#    method_names = data[xs[0]].keys()
-#    ys = collections.defaultdict(list)
-#    for x in xs:
-#        for method in method_names:
-#            ys[method].append(data[x][method])
-#
-#    fig = plt.figure()
-#    for method in method_names:
-#        print xs
-#        print ys[method]
-#        plt.plot(xs, ys[method])
-#
-#    plt.yscale('log')
-#    plt.xlabel('Hilbert Curve Order')
-#    plt.ylabel('log(verification time)')
-#    plt.show()
-#
 
 def save_data(data):
     with open("performance-data-" + str(time.time()), 'w') as f:
@@ -71,4 +52,5 @@ def main():
     fin()
 
 
-main()
+if __name__ == "__main__":
+    main()
