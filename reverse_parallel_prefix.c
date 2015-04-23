516b0b556484a57ccf7e46a9003a5c74d8b4106f
Matt Gautreau <mgaut72@gmail.com>
2015-04-23 09:35:12 -0700

    add reverse parallel prefix, partial implementation

---
+++ reverse_parallel_prefix.c
@@ -0,0 +1,3 @@
+#include "reverse_parallel_prefix.h"
+
+unsigned hil_s_from_xy_ls(unsigned x, unsigned y, int n);
---
+++ reverse_parallel_prefix.h
@@ -0,0 +1,6 @@
+#ifndef H_REVERSE_PARALLEL_PREFIX
+#define H_REVERSE_PARALLEL_PREFIX
+
+unsigned hil_s_from_xy_pp(unsigned x, unsigned y, int n);
+
+#endif
